5eacb43a8dfa3878fdf69a91cb4fe46c0ddf7165
Piyushshind <piyushshinde.199@gmail.com>
2024-10-14 10:07:24 +0530

    angular practice

---
+++ src/app/context.ts
@@ -0,0 +1,9 @@
+export class inventotyData {
+    bookID: number = 0;
+    bookTitle: String = "";
+
+    autherName: String = "";
+    sellingPrice: number = 0;
+
+    bookCatagory: String = "";
+}
